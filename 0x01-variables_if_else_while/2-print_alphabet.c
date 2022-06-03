ef8daacafe91779cfe264e627c17f6067a4ef9b9
Feven Liben <libsamfev@gmail.com>
2022-06-03 12:05:53 -0700

    file added 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - prints alphabet
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	
 	int i;
 	for (i = 0; i < 26; i++)
 	{
