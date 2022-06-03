1c390e43c5976d108b4e281847acfebefbb74bd8
Feven Liben <libsamfev@gmail.com>
2022-06-03 13:16:27 -0700

    filr 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints 0 - 9 and a - f
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i = 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i = 103; i++)
+	{
+		putchar(i):
+	}
+	putchar('\n');
+	return (0);
+}
