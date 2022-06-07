9f7df1a4dfddd782ec8a70534f7501215b9e6b67
Feven Liben <libsamfev@gmail.com>
2022-06-07 14:54:54 -0700

    6 file add

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - shows the absolute value
+ *
+ * @i: is an integer input
+ *
+ * Return: absolute value
+ */
+int _abs(int i)
+{
+	if (i > 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
