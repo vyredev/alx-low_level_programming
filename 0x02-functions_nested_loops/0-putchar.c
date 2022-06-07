8924c61f76d9e7bf3cbdf08a68c4ec173407f5fe
Feven Liben <libsamfev@gmail.com>
2022-06-07 10:42:15 -0700

    main.h and file 0- added

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * main - Entry point
+ * main - Entry point, prints _putchar
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("_putchar\n");
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+
+	}
+	_putchar('\n');
 	return (0);
 }
