482cf368f3e538579024c643c98141a8c8b1f8d2
Feven Liben <libsamfev@gmail.com>
2022-06-12 10:19:53 -0700

    file add

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplies integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
