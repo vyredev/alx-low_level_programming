496b0369a42eb4e706c36c91ab441aebc29d65b1
Feven Liben <libsamfev@gmail.com>
2022-06-12 10:00:19 -0700

    file add

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,7 +4,7 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-void print_number(void);
+void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
