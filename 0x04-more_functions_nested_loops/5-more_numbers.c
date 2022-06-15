819c1fef313ddfc4813b9eceab148dfc1c5a808d
Feven Liben <libsamfev@gmail.com>
2022-06-15 09:12:37 -0700

    file add

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,13 +10,11 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (ch = 0; i < 15; ch++)
+		for (i = 0; i < 10; i++)
 		{
 			if (ch >= 10)
-			{
 				_putchar((ch / 10) + 48);
-				_putchar((ch % 10) + 48);
-			}
+			_putchar((ch % 10) + 48);
 		}
 		_putchar('\n');
 	}
