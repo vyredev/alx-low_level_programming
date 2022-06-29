0371133b79ebe2fe5477b9096bafc93c6e5567e4
Feven Liben <libsamfev@gmail.com>
2022-06-29 01:57:53 -0700

    file add

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * string_toupper - change all lowercase
- * to upper case
+ * string_toupper - change all lowercase to uppercase
  * @n: pointer
  *
  * Return: n
  */
 char *string_toupper(char *n)
 {
-	int i:
+	int i;
 
 	i = 0;
 	while (n[i] != '\0')
