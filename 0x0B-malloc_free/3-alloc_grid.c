53eb42932fb2d9eedc7d9863f0e3ef655134b50d
Feven Liben <libsamfev@gmail.com>
2022-07-01 08:05:24 -0700

    fic

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = mallow(sizeof(int) * width);
+		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
 		{
 			while (--i >= 0)
