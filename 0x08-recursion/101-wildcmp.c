d29a0ef0043971e3fcafa51d736afe8902b209cb
Feven Liben <libsamfev@gmail.com>
2022-07-01 03:26:47 -0700

    fix

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -4,6 +4,7 @@
  * wildcmp - copaares two strings
  * @s1: string 1
  * @s2: string 2. May contain * as a special character
+ * Return: 1 if identical, 0 if not
  */
 int wildcmp(char *s1, char *s2)
 {
