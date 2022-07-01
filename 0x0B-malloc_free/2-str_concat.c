37a614f17ff3e6b747bf39d427d6cdbecbe40370
Feven Liben <libsamfev@gmail.com>
2022-07-01 08:00:43 -0700

    fic

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,7 +18,7 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
-	
+
 	for (j = 0; s2[j] != '\0'; j++)
 		;
 
@@ -37,5 +37,5 @@ char *str_concat(char *s1, char *s2)
 	for (j = 0; j <= limit; k++, j++)
 		strout[k] = s2[j];
 
-	return(strout);
+	return (strout);
 }
