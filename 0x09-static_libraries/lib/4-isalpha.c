c3b4c3967faaf70d7c483aefd2d07ac02dc4303b
Feven Liben <libsamfev@gmail.com>
2022-07-20 08:33:13 -0700

    nre project with files

---
+++ 0x09-static_libraries/lib/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - recognizes alphabet
+ *
+ * @c: ASCII character
+ *
+ * Return: 1 if alphabet, 0 if other
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
